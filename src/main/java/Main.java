1c1b89670addeece8ab847b06df0c0da8e877566
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-03-22 14:57:18 +0700

    Add & Update | Project Initialized and Login Form

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author misterfocusth
+ */
+import forms.LoginForm;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        LoginForm loginForm = new LoginForm();
+        loginForm.setVisible(true);
+    }
+}
