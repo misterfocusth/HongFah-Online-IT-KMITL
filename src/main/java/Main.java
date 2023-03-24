7c28b95f1400033cd0544c968c5dfbdc4e71d020
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-03-24 17:22:23 +0700

    Update Main.java main()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,6 +15,7 @@ public class Main {
         System.out.println("Hello, World!");
 
         NewLoginForm newLoginForm = new NewLoginForm();
+        newLoginForm.setLocationRelativeTo(null);
         newLoginForm.setVisible(true);
     }
 }
