5038de4e3c2405c6c356850e16fb30ddb384e222
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-03-25 22:53:24 +0700

    Update | Database & StudentDatabase Services

---
+++ src/main/java/database/StudentDatabase.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.WriteResult;
+import static firebase.Firebase.db;
+import java.util.HashMap;
+import java.util.Map;
+import user.Student;
+
+/**
+ *
+ * @author misterfocusth
+ */
+public class StudentDatabase {
+
+    public Student getStudentById(int studentId) {
+        return null;
+    }
+
+    public synchronized boolean addNewStudent(HashMap<String, String> studentData) {
+        // Personal Data
+        String thNameTitle = studentData.get("thNameTitle");
+        String thFirstName = studentData.get("thFirstName");
+        String thLastName = studentData.get("thLastName");
+        String enNameTitle = studentData.get("enNameTitle");
+        String enFirstName = studentData.get("enFirstName");
+        String enLastName = studentData.get("enLastName");
+        String identificationNumber = studentData.get("identificationNumber");
+        String email = studentData.get("email");
+        String phoneNumber = studentData.get("phoneNumber");
+        String contactAddress = studentData.get("contactAddress");
+        String dobData = studentData.get("dobData");
+        String dobMonth = studentData.get("dobMonth");
+        String dobYear = studentData.get("dobYear");
+        String gender = studentData.get("gender");
+
+        // Education Data
+        String studentId = studentData.get("studentId");
+        String classYear = studentData.get("classYear");
+        String generation = studentData.get("generation");
+        String schoolName = studentData.get("schoolName");
+        String campusName = studentData.get("campusName");
+        String majorName = studentData.get("majorName");
+        String programeName = studentData.get("programeName");
+
+        DocumentReference docRef = db.collection("student").document(studentId);
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("studentId", studentId);
+        data.put("thNameTitle", thNameTitle);
+        data.put("thFirstName", thFirstName);
+        data.put("thLastName", thLastName);
+        data.put("enNameTitle", enNameTitle);
+        data.put("enFirstName", enFirstName);
+        data.put("enLastName", enLastName);
+        data.put("identificationNumber", identificationNumber);
+        data.put("email", email);
+        data.put("phoneNumber", phoneNumber);
+        data.put("contactAddress", contactAddress);
+        data.put("dobData", dobData);
+        data.put("dobMonth", dobMonth);
+        data.put("dobYear", dobYear);
+        data.put("gender", gender);
+        data.put("classYear", classYear);
+        data.put("generation", generation);
+        data.put("schoolName", schoolName);
+        data.put("campusName", campusName);
+        data.put("majorName", majorName);
+        data.put("programeName", programeName);
+
+        ApiFuture<WriteResult> result = docRef.set(data);
+
+        return true;
+    }
+}
---
+++ src/main/java/firebase/Firebase.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+/**
+ *
+ * @author misterfocusth
+ */
+public class Firebase {
+
+    public static Firestore db = null;
+
+    public static void initFirebase() throws FileNotFoundException, IOException, URISyntaxException {
+        System.out.println(new Firebase().getServiceAccount());
+        InputStream serviceAccount = new FileInputStream(new Firebase().getServiceAccount());
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(credentials)
+                .build();
+        FirebaseApp.initializeApp(options);
+        Firebase.db = FirestoreClient.getFirestore();
+    }
+
+    public File getServiceAccount() throws URISyntaxException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource("firebase/hongfah-online-it-kmitl-firebase-adminsdk-wbpdq-20d971f595.json");
+
+        if (resource == null) {
+            throw new IllegalArgumentException("File not found!" + resource.toURI());
+        } else {
+            return new File(resource.toURI());
+        }
+
+    }
+}
