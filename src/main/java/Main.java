dc312acdc04010872e8f582bd557496014c1f6fc
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-03-25 22:50:37 +0700

    Call initFirebase() when main() is run at start application time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,25 @@
  *
  * @author misterfocusth
  */
+import firebase.Firebase;
 import forms.NewLoginForm;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        try {
+            Firebase.initFirebase();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         NewLoginForm newLoginForm = new NewLoginForm();
         newLoginForm.setLocationRelativeTo(null);
