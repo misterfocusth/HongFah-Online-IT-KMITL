d92cf071b4112cc0b55a8b1458e0a6871454db23
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-03-25 22:51:22 +0700

    Add class attributes for User and Student class

--- src/main/java/user/Student.java
+++ src/main/java/user/Student.java
@@ -10,4 +10,68 @@ package user;
  */
 public class Student extends User {
 
+    private String classYear;
+    private String generation;
+    private String schoolName;
+    private String campusName;
+    private String majorName;
+    private String programeName;
+    private String studentId;
+
+    public String getClassYear() {
+        return classYear;
+    }
+
+    public void setClassYear(String classYear) {
+        this.classYear = classYear;
+    }
+
+    public String getGeneration() {
+        return generation;
+    }
+
+    public void setGeneration(String generation) {
+        this.generation = generation;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getCampusName() {
+        return campusName;
+    }
+
+    public void setCampusName(String campusName) {
+        this.campusName = campusName;
+    }
+
+    public String getMajorName() {
+        return majorName;
+    }
+
+    public void setMajorName(String majorName) {
+        this.majorName = majorName;
+    }
+
+    public String getProgrameName() {
+        return programeName;
+    }
+
+    public void setProgrameName(String programeName) {
+        this.programeName = programeName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
 }
--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -8,10 +8,16 @@ package user;
  *
  * @author misterfocusth
  */
-public abstract class User {
+public class User {
 
     private String username;
-    private String hashedPassword;
+    private String thNameTitile, enNameTitle;
+    private String thFirstName, thLastName;
+    private String enFirstName, enLastName;
+    private String identificationNumber;
+    private String email, phoneNumber, contactAddress;
+    private String dobDate, dobMonth, dobYear;
+    private String gender;
 
     public String getUsername() {
         return username;
@@ -21,12 +27,116 @@ public abstract class User {
         this.username = username;
     }
 
-    public String getHashedPassword() {
-        return hashedPassword;
+    public String getThNameTitile() {
+        return thNameTitile;
     }
 
-    public void setHashedPassword(String hashedPassword) {
-        this.hashedPassword = hashedPassword;
+    public void setThNameTitile(String thNameTitile) {
+        this.thNameTitile = thNameTitile;
+    }
+
+    public String getEnNameTitle() {
+        return enNameTitle;
+    }
+
+    public void setEnNameTitle(String enNameTitle) {
+        this.enNameTitle = enNameTitle;
+    }
+
+    public String getThFirstName() {
+        return thFirstName;
+    }
+
+    public void setThFirstName(String thFirstName) {
+        this.thFirstName = thFirstName;
+    }
+
+    public String getThLastName() {
+        return thLastName;
+    }
+
+    public void setThLastName(String thLastName) {
+        this.thLastName = thLastName;
+    }
+
+    public String getEnFirstName() {
+        return enFirstName;
+    }
+
+    public void setEnFirstName(String enFirstName) {
+        this.enFirstName = enFirstName;
+    }
+
+    public String getEnLastName() {
+        return enLastName;
+    }
+
+    public void setEnLastName(String enLastName) {
+        this.enLastName = enLastName;
+    }
+
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getContactAddress() {
+        return contactAddress;
+    }
+
+    public void setContactAddress(String contactAddress) {
+        this.contactAddress = contactAddress;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDobDate() {
+        return dobDate;
+    }
+
+    public void setDobDate(String dobDate) {
+        this.dobDate = dobDate;
+    }
+
+    public String getDobMonth() {
+        return dobMonth;
+    }
+
+    public void setDobMonth(String dobMonth) {
+        this.dobMonth = dobMonth;
+    }
+
+    public String getDobYear() {
+        return dobYear;
+    }
+
+    public void setDobYear(String dobYear) {
+        this.dobYear = dobYear;
     }
 
 }
