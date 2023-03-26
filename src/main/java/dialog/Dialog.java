3e69f2847bab76f625b6f0d4b0c50e6427dc13d1
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-03-26 10:22:54 +0700

    Add | Dialog & InfoDialog class for creating JOtionPane

---
+++ src/main/java/dialog/Dialog.java
@@ -0,0 +1,57 @@
+package dialog;
+
+import java.awt.*;
+
+public abstract class Dialog {
+    private Label message;
+    private String dialogTitle;
+    private final Font font;
+    private Component parentComponent;
+
+    public Dialog(Component parentComponent, String dialogTitle, String message) {
+        this.message = new Label(message);
+        this.dialogTitle = dialogTitle;
+        this.parentComponent = parentComponent;
+
+        this.font = new Font("Tahoma", Font.BOLD, 14);
+        this.message.setFont(font);
+    }
+
+    public Dialog(String dialogTitle, String message) {
+        this(null, dialogTitle, message);
+    }
+
+    public Dialog() {
+        this(null, null, null);
+    }
+
+    public Label getMessage() {
+        return message;
+    }
+
+    public void setMessage(Label message) {
+        this.message = message;
+    }
+
+    public String getDialogTitle() {
+        return dialogTitle;
+    }
+
+    public void setDialogTitle(String dialogTitle) {
+        this.dialogTitle = dialogTitle;
+    }
+
+    public Font getFont() {
+        return font;
+    }
+
+    public Component getParentComponent() {
+        return parentComponent;
+    }
+
+    public void setParentComponent(Component parentComponent) {
+        this.parentComponent = parentComponent;
+    }
+
+    public abstract void show();
+}
---
+++ src/main/java/dialog/InfoDialog.java
@@ -0,0 +1,27 @@
+package dialog;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class InfoDialog extends Dialog {
+
+    public InfoDialog() {
+
+    }
+
+    public InfoDialog(Component parentComponent, String message, String dialogTitle) {
+        super(parentComponent, dialogTitle, message);
+    }
+
+    public InfoDialog(String dialogTitle, String message) {
+        super(dialogTitle, message);
+    }
+
+    @Override
+    public void show() {
+        Label message = this.getMessage();
+        String dialogTitle = this.getDialogTitle();
+        Component parentComponent = this.getParentComponent();
+        JOptionPane.showMessageDialog(parentComponent, message, dialogTitle, JOptionPane.INFORMATION_MESSAGE);
+    }
+}
